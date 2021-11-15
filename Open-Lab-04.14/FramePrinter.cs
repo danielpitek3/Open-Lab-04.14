0b56d96a07e6e42d51c74328da7b4f23aff5fc16
Daniel Pitek <Daniel813245@outlook.sk>
2021-11-15 18:37:34 +0100

    hotovo

--- Open-Lab-04.14/FramePrinter.cs
+++ Open-Lab-04.14/FramePrinter.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace Open_Lab_04._14
 {
@@ -6,7 +7,22 @@ namespace Open_Lab_04._14
     {
         public void Print(string[] strings)
         {
-            throw new NotImplementedException();
+            string longest_String = strings.OrderByDescending(s => s.Length).First();
+            int frame_length = longest_String.Length + 4; 
+
+            string borders = "";
+            borders = borders.PadRight(frame_length, '');
+            Console.WriteLine(borders);
+
+            foreach (var str in strings)
+            {
+                string new_String = " " + str;
+                new_String = new_String.PadRight(frame_length - 1, ' ');
+                new_String += "*";
+                Console.WriteLine(new_String);
+            }
+
+            Console.WriteLine(borders);
         }
     }
 }
